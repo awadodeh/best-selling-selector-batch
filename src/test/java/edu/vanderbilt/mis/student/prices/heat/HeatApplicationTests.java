82f4823731891bb236ad46dfcb8711d9cfc7ff88
owdaa <awadodeh@gmail.com>
2017-10-05 13:37:39 -0500

    initial commit

---
+++ src/test/java/edu/vanderbilt/mis/student/prices/heat/HeatApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.vanderbilt.mis.student.prices.heat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HeatApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
